6d0f0dac9f736ca871bb6004837d87bda3997c63
Hai <hait20490@gmail.com>
2025-03-24 07:34:39 +0700

    EC-23 API GET POST /orders (EC-15)

--- server/src/server.js
+++ server/src/server.js
@@ -7,6 +7,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 dotenv.config();
 connectDB();
@@ -20,6 +21,7 @@ app.use(morgan("dev"));
 app.use("/api/v1/users", userRoutes);
 app.use(`/api/v1/categories`, categoryRoutes);
 app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/orders", orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
