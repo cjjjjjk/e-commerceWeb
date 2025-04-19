9584170edff516e01315ce60a8ee70a2e0e2af75
Hiruby <truongduchai4@gmail.com>
2025-04-19 23:32:21 +0700

    EC-39 Local storing in work

--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -1,14 +1,11 @@
-"use client"
-
 import './signin.css'
 
-
+import { useState } from 'react';
 import {auth, provider, signInWithPopup} from '../../shared/services/auth'
 import {  useNavigate } from 'react-router-dom';
 import { UserCredential } from 'firebase/auth';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import { useForm } from 'react-hook-form';
 import { addToast } from 'shared/components/toast/toastSlice';
 
 const API_URL = process.env.REACT_APP_API_URL
@@ -20,22 +17,34 @@ interface AuthUserCredential extends UserCredential {
   }
 
 function SignIn() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
     const showToast = (message: string, type: "success" | "error" | "info", link?: string) => {
       dispatch(addToast({ message, type , link}));
     };
-  
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleLogin = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(`${API_URL}/users/login`, { email, password });
+            const { token, data } = res.data;
+            console.log("SIGNIN RES:", res.data);
+            if (res.data.code === "success") {
+                localStorage.setItem('token', token);
+                showToast("Đăng nhập thành công", "success");
+                if(data.role === "admin") showToast("Bạn là ADMIN", "info", '/admin');
+                navigate("/member");
+            } else {
+                showToast("Đăng nhập thất bại", "error");
+            }
+        } catch (err) {
+            console.error(err);
+            showToast("Đăng nhập thất bại", "error");
+        }
+    }
 
-    const onSubmit = (data: any) => {
-        localStorage.setItem(data.email, JSON.stringify({ 
-            name: data.name, password: data.password 
-        }));
-        console.log(JSON.parse(localStorage.getItem(data.email) || '{}'));
-      };
-    
     // SignIn with GG ====================================== author: Hai
     const GGSignIn = async () => {
         if(!auth || !provider) {
@@ -73,26 +82,26 @@ function SignIn() {
 
     return ( 
         <div className='sigin-full-container h-100 w-100 d-flex justify-content-center align-items-center'>
-            <form className="signin-form" onSubmit={handleSubmit(onSubmit)}>
+            <form className="signin-form" >
                 <div className="sigin-container d-flex flex-column align-items-baseline gap-4">
-                    <h1>ĐĂNG NHẬP</h1>
+                    <h1 className="mt-4">ĐĂNG NHẬP</h1>
                     <fieldset>
-                        <input className="input" type="text" placeholder=""  {...register("name")} />
+                        <input className="input email" type="text" placeholder="" />
                         <span className="label">Email/Số điện thoại</span>
                     </fieldset>
                     <fieldset>
-                        <input className="input" type="password" placeholder="" {...register("email", { required: true })} />
+                        <input className="input password" type="password" placeholder="" />
                         <span className="label">Mật khẩu</span>
                         <p className="mt-4">Mật khẩu phải có từ 8 đến 20 kí tự bao gồm cả chữ và số.</p>
                     </fieldset>
                     <button 
-                        type="submit"
                         className="signin p-4 d-flex gap-3 justify-content-center align-items-center border-0"
+                        onClick={handleLogin}
                     >Đăng nhập</button>
-                    <span>Sử dụng tài khoản Google để đăng nhập. </span>
+                    <span >Sử dụng tài khoản Google để đăng nhập. </span>
                     <button
-                        className="signin p-4 d-flex gap-3 justify-content-center align-items-center border-0"
-                        onClick={handleSubmit(GGSignIn)} 
+                        className="signin mt-1 p-4 d-flex gap-3 justify-content-center align-items-center border-0"
+                        onClick={() => GGSignIn()}
                     >
                         <i className="pi pi-google"></i>
                         Đăng nhập với Google
